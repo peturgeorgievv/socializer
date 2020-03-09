fd717ccadaa22ad96793cf0e575af7eb29010768
peturgeorgievv <peturgeorgievv@gmail.com>
2020-03-09 22:31:37 +0200

    Added styling and real data for user followers/following

--- src/components/features/users/FollowersModal.tsx
+++ src/components/features/users/FollowersModal.tsx
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { UserData } from '../../../models/users/UserData';
+import firebase from '../../../config/firebaseService';
 
-class FollowersModal extends Component<any, any> {
+type FollowersModalProps = {
+  userData: UserData;
+  show: boolean;
+  handleClose: any;
+}
+
+type FollowersModalState = {
+  filteredUsers: any[];
+}
+
+class FollowersModal extends Component<FollowersModalProps, FollowersModalState> {
+  _isMounted: boolean;
+
+  constructor(props: FollowersModalProps) {
+    super(props);
+
+    this._isMounted = true;
+    this.state = {
+      filteredUsers: [],
+    }
+  }
+
+  componentDidMount = () => {
+    this.getUserData();
+  }
+
+  componentWillUnmount = () => {
+    this._isMounted = false;
+  }
+
+  getUserData = () => {
+    firebase.firestore()
+      .collection('users')
+      .onSnapshot(snapshot => {
+        if (this._isMounted) {
+          this.setState({ filteredUsers: [] });
+        }
+        snapshot.forEach(user => {
+          if (this._isMounted && this.props.userData.followers.find((userData: any) => userData.userDocumentId === user.data().documentId)) {
+            this.setState(prevState => ({ 
+              filteredUsers: [...prevState.filteredUsers, user.data()]
+            }))
+          }
+        })
+      })
+  }
  
   render = () => {
     const showHideClassName = this.props.show ? 'modal display-block' : 'modal display-none';
-    // const showHideUnlikeButton = this.props.imgData.likes && this.props.imgData.likes.find(() => this.props.currentUser.documentId) ? 'heart-icon-liked' : 'hide-button'; 
-    // const showHideLikeButton = this.props.imgData.likes && this.props.imgData.likes.find(() => this.props.currentUser.documentId) ? 'hide-button' : 'heart-icon-unliked'; 
 
     return (
       <div className={showHideClassName}>
         <section className="modal-main-followers">
-          {this.props.userData.followers.length && this.props.userData.followers.map((follower: any, index: any) => {
-            return (<Link to={`/users/${follower.userDocumentId}`} key={index}>
-              <div>
-                <img className="modal-avatar" src='' alt="modal"/>
-                {`${follower.firstName} ${follower.lastName}`}
+        <h3>Followers</h3>
+          {this.state.filteredUsers.length && this.state.filteredUsers.map((follower: UserData, index: number) => {
+            return (
+            <Link to={`/users/${follower.documentId}`} key={index} className="link-container">
+              <div className="follower-container">
+                <img className="modal-avatar" src={follower.profilePhotoUrl} alt="avatar"/>
+                <span className="followers-name">{`${follower.firstName} ${follower.lastName}`}</span>
               </div>
             </Link>
           )})}
-          <button onClick={this.props.handleClose}>close</button>
+          <span onClick={this.props.handleClose} className="close-modal-icon"></span>
         </section>
       </div>
     );
--- src/components/features/users/FollowingModal.tsx
+++ src/components/features/users/FollowingModal.tsx
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import firebase from '../../../config/firebaseService';
+import { UserData } from '../../../models/users/UserData';
 
-class FollowingModal extends Component<any, any> {
+type FollowingModalProps = {
+  userData: UserData;
+  show: boolean;
+  handleClose: any;
+}
+
+type FollowingModalState = {
+  filteredUsers: any[];
+}
+
+
+class FollowingModal extends Component<FollowingModalProps, FollowingModalState> {
+  _isMounted: boolean;
+
+  constructor(props: FollowingModalProps) {
+    super(props);
+
+    this._isMounted = true;
+    this.state = {
+      filteredUsers: [],
+    }
+  }
+
+  componentDidMount = () => {
+    this.getUserData();
+  }
+
+  componentWillUnmount = () => {
+    this._isMounted = false;
+  }
+
+  getUserData = () => {
+    firebase.firestore()
+      .collection('users')
+      .onSnapshot(snapshot => {
+        if (this._isMounted) {
+          this.setState({ filteredUsers: [] });
+        }
+        snapshot.forEach(user => {
+          if (this._isMounted && this.props.userData.following.find((userData: any) => userData.userDocumentId === user.data().documentId)) {
+            this.setState(prevState => ({ 
+              filteredUsers: [...prevState.filteredUsers, user.data()]
+            }))
+          }
+        })
+      })
+  }
  
   render = () => {
     const showHideClassName = this.props.show ? 'modal display-block' : 'modal display-none';
-    // const showHideUnlikeButton = this.props.imgData.likes && this.props.imgData.likes.find(() => this.props.currentUser.documentId) ? 'heart-icon-liked' : 'hide-button'; 
-    // const showHideLikeButton = this.props.imgData.likes && this.props.imgData.likes.find(() => this.props.currentUser.documentId) ? 'hide-button' : 'heart-icon-unliked'; 
 
     return (
       <div className={showHideClassName}>
-        <section className="modal-main-followers">
-          {this.props.userData.following.length && this.props.userData.following.map((follower: any, index: any) => {
-            return (<Link to={`/users/${follower.userDocumentId}`} key={index}>
-              <div>
-                <img className="modal-avatar" src='' alt="modal"/>
-                {`${follower.firstName} ${follower.lastName}`}
+        <section className="modal-main-following">
+        <h3>Following</h3>
+          {this.state.filteredUsers.length && this.state.filteredUsers.map((following: UserData, index: number) => {
+            return (
+            <Link to={`/users/${following.documentId}`} key={index} className="link-container">
+              <div className="following-container">
+                <img className="modal-avatar" src={following.profilePhotoUrl} alt="avatar"/>
+                <span className="following-name">{`${following.firstName} ${following.lastName}`}</span>
               </div>
             </Link>
           )})}
-          <button onClick={this.props.handleClose}>close</button>
+          <span onClick={this.props.handleClose} className="close-modal-icon"></span>
         </section>
       </div>
     );
